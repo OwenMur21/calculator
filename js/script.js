2efa089db1d2314a84362ab3aebd0e95ac0e1686
Owen Muriithi <owenmuriithi@gmail.com>
2018-04-24 15:57:29 +0300

    Adds subtraction, multiplication and division

--- js/script.js
+++ js/script.js
@@ -5,3 +5,24 @@ var number1=parseInt(prompt("Enter a number:"));
 var number2=parseInt(prompt("Enter another number:"));
 var result=add(number1,number2);
 alert(result);
+var subtract=function(number1,number2) {
+  return number1-number2;
+};
+var number1=parseInt(prompt("Enter a number:"));
+var number2=parseInt(prompt("Enter another number:"));
+var result2=subtract(number1,number2);
+alert(result2);
+var multiply=function(number1,number2) {
+  return number1*number2;
+};
+var number1=parseInt(prompt("Enter a number:"));
+var number2=parseInt(prompt("Enter another number:"));
+var result3=multiply(number1,number2);
+alert(result3);
+var divide=function(number1,number2) {
+  return number1/number2;
+};
+var number1=parseInt(prompt("Enter a number:"));
+var number2=parseInt(prompt("Enter another number:"));
+var result4=divide(number1,number2);
+alert(result4);
